d58a43da14dc27a99209dc7da341cae70b9f8bff
Michael Deng <michaeldeng18@gmail.com>
2016-10-31 17:52:46 -0700

    Deploy app on heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfiguration(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://michael:mypassword@localhost/game'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class TestConfiguration(BaseConfiguration):
     TESTING = True
